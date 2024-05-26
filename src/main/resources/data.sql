003113efd9f7d4397dfb2506085b89e345d5178e
jjyeon <wodus9193@naver.com>
2024-05-26 23:50:47 +0900

    #10 - 테스트 데이터를 data.sql로 추가
    이걸로 테스트 통과함

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into article (title,content,hashtag,created_by,modified_by,created_at,modified_at)
+values ('행복하다.','나는 행복하다.','hello','정재연','정재연','2023-01-01','2024-12-12');
+insert into article (title,content,hashtag,created_by,modified_by,created_at,modified_at)
+values ('행복하다.','나는 행복하다.','hello','정재연','정재연','2023-01-01','2024-12-12');
+insert into article (title,content,hashtag,created_by,modified_by,created_at,modified_at)
+values ('행복하다.','나는 행복하다.','hello','정재연','정재연','2023-01-01','2024-12-12');
+insert into article (title,content,hashtag,created_by,modified_by,created_at,modified_at)
+values ('행복하다.','나는 행복하다.','hello','정재연','정재연','2023-01-01','2024-12-12');
+insert into article_comment (content,article_id,created_by,modified_by,created_at,modified_at)
+values ('행복하다.',1,'정재연','정재연','2023-01-01','2024-12-12');
+insert into article_comment (content,article_id,created_by,modified_by,created_at,modified_at)
+values ('행복하다.',1,'정재연','정재연','2023-01-01','2024-12-12');
+insert into article_comment (content,article_id,created_by,modified_by,created_at,modified_at)
+values ('행복하다.',1,'정재연','정재연','2023-01-01','2024-12-12');
